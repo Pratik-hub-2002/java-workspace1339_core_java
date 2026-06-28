8fafa7e3572d381c957f2daf42c844f8334deeef
Pratik-Hub-2002 <praut3022@gmail.com>
2026-06-28 16:30:31 +0530

    for loops practice Q.50

--- Loops/src/Practice/A.java
+++ Loops/src/Practice/A.java
@@ -16,9 +16,286 @@ public class A {
 //		for (int i = -56; i <= 57; i++) {
 //			System.out.println(i);
 //		}
-		
-		for (int i=10; i<=50; i*=2) {
-			System.out.println(i);
-		}
+
+//		for (int i=10; i<=50; i*=2) {
+//			System.out.println(i);
+//		}
+
+//		Increasing Order
+//		-100 to 100
+//		for (int i = -100; i <= 100; i++) {
+//			System.out.println(i);
+//		}
+
+//		-50 to 50
+//		for(int i=-50; i<=50 ; i++) {
+//			System.out.println(i);
+//		}
+
+//		-25 to 75
+//		for(int i=-25; i<=75; i++) {
+//			System.out.println(i);
+//		}
+
+//		0 to 100
+//		for (int i = 0; i <= 100; i++) {
+//			System.out.println(i);
+//		}
+
+//		1 to 50
+//		for (int i = 1; i <= 50; i++) {
+//			System.out.println(i);
+//		}
+
+//		10 to 100
+//		for (int i = 10; i <= 100; i++) {
+//			System.out.println(i);
+//		}
+
+//		25 to 250
+//		for(int i = 25; i <= 250; i++) {
+//			System.out.println(i);
+//		}
+
+//		100 to 200
+//		for (int i = 100; i <= 200; i++) {
+//			System.out.println(i);
+//		}
+
+//		-200 to -100
+//		for (int i = -200; i <= -100; i++) {
+//			System.out.println(i);
+//		}
+
+//		-500 to 0
+//		for (int i = -500; i <= 0; i++) {
+//			System.out.println(i);
+//		}
+
+//		Decreasing Order
+//		100 to 0
+//		for (int i = 100; i >= 0; i--) {
+//			System.out.println(i);
+//		}
+
+//		50 to -50
+//		for (int i = 50; i >= -50; i--) {
+//			System.out.println(i);
+//		}
+
+//		200 to 100
+//		for (int i = 200; i >= 100; i--) {
+//			System.out.println(i);
+//		}
+
+//		0 to -100
+//		for (int i = 0; i >= -100; i--) {
+//			System.out.println(i);
+//		}
+
+//		-10 to -100
+//		for(int i=-10;i>=-100;i--) {
+//			System.out.println(i);
+//		}
+
+//		500 to 0
+//		for(int i=500;i>=0;i--) {
+//			System.out.println(i);
+//		}
+
+//		1000 to 900
+//		for (int i = 1000; i >= 900; i--) {
+//			System.out.println(i);
+//		}
+
+//		75 to 25
+//		for (int i = 75; i >= 25; i--) {
+//			System.out.println(i);
+//		}
+
+//		20 to -20
+//		for (int i = 20; i >= -20; i--) {
+//			System.out.println(i);
+//		}
+
+//		150 to 50
+//		for (int i = 150; i >= 50; i--) {
+//			System.out.println(i);
+//		}
+
+//		Step by 2
+//		0 to 50 (increment by 2)
+//		for (int i = 0; i <= 50; i += 2) {
+//			System.out.println(i);
+//		}
+
+//		-20 to 20 (increment by 2)
+//		for(int i=-20; i<=20; i+=2) {
+//			System.out.println(i);
+//		}
+
+//		100 to 200 (increment by 2)
+//		for (int i = 100; i <= 200; i += 2) {
+//			System.out.println(i);
+//		}
+
+//		50 to 0 (decrement by 2)
+//		for (int i = 50; i >= 0; i -= 2) {
+//			System.out.println(i);
+//		}
+
+//		-100 to -50 (increment by 2)
+//		for(int i=-100; i<=-50; i+=2) {
+//			System.out.println(i);
+//		}
+
+//		Step by 5
+//		0 to 100 (increment by 5)
+//		for (int i = 0; i <= 100; i += 5) {
+//			System.out.println(i);
+//		}
+
+//		100 to 0 (decrement by 5)
+//		for (int i = 100; i >= 0; i -= 5) {
+//			System.out.println(i);
+//		}
+
+//		-50 to 50 (increment by 5)
+//		for (int i = -50; i <= 50; i += 5) {
+//			System.out.println(i);
+//		}
+
+//		500 to 600 (increment by 5)
+//		for (int i = 500; i <= 600; i += 5) {
+//			System.out.println(i);
+//		}
+
+//		250 to 150 (decrement by 5)
+//		for (int i = 250; i >= 150; i -= 5) {
+//			System.out.println(i);
+//		}
+
+//		Step by 10
+//		0 to 100 (increment by 10)
+//		for(int i=0;i<=100;i+=10) {
+//			System.out.println(i);
+//		}
+
+//		100 to 0 (decrement by 10)
+//		for (int i = 100; i >= 0; i -= 10) {
+//			System.out.println(i);
+//		}
+//
+		//// -100 to 100 (increment by 10)
+//		for (int i = -100; i <= 100; i += 10) {
+//			System.out.println(i);
+//		}
+//
+		//// 500 to 1000 (increment by 10)
+//		for (int i = 500; i <= 1000; i += 10) {
+//			System.out.println(i);
+//		}
+//
+		//// 1000 to 500 (decrement by 10)
+//		for (int i = 1000; i >= 500; i -= 10) {
+//			System.out.println(i);
+//		}
+//
+		//// Even Numbers / 2 to 100
+//		for (int i = 2; i <= 100; i++) {
+//			if (i % 2 == 0) {
+//				System.out.println(i);
+//			}
+//		}
+
+//		-50 to 50
+//		for (int i = -50; i <= 50; i++) {
+//			if (i % 2 == 0) {
+//				System.out.println(i);
+//			}
+//		}
+
+//		100 to 200
+//		for (int i = 100; i <= 200; i++) {
+//			if (i % 2 == 0) {
+//				System.out.println(i);
+//			}
+//		}
+
+//		200 to 100 (reverse)
+//		for (int i = 200; i >= 100; i--) {
+//			if (i % 2 == 0) {
+//				System.out.println(i);
+//			}
+//		}
+
+//		0 to 500
+//		for (int i = 0; i <= 500; i++) {
+//			if (i % 2 == 0) {
+//				System.out.println(i);
+//			}
+//		}
+
+//		Odd Numbers
+//		1 to 99
+//		for (int i = 1; i <= 99; i++) {
+//			if (i % 2 == 1) {
+//				System.out.println(i);
+//			}
+//		}
+
+//		-49 to 49
+//		for (int i = -49; i <= 49; i++) {
+//			if (i % 2 == 1 || i % -2 == -1) {
+//				System.out.println(i);
+//			}
+//		}
+
+//		101 to 199
+//		for (int i = 101; i <= 199; i++) {
+//			if (i % 2 == 1) {
+//				System.out.println(i);
+//			}
+//		}
+
+//		99 to 1 (reverse)
+//		for (int i = 99; i >= 1; i--) {
+//			if (i % 2 == 1) {
+//				System.out.println(i);
+//			}
+//		}
+
+//		1 to 501
+//		for (int i = 1; i <= 501; i++) {
+//			if (i % 2 == 1) {
+//				System.out.println(i);
+//			}
+//		}
+
+//		Mixed
+//		-1000 to 1000
+//		for (int i = -1000; i <= 1000; i++) {
+//			System.out.println(i);
+//		}
+
+//		-365 to 365
+//		for (int i = -365; i <= 365; i++) {
+//			System.out.println(i);
+//		}
+
+//		-999 to 999
+//		for (int i = -999; i <= 999; i++) {
+//			System.out.println(i);
+//		}
+
+//		2020 to 2050
+//		for (int i = 2020; i <= 2050; i++) {
+//			System.out.println(i);
+//		}
+
+//		-2025 to 2025
+//		for (int i = -2025; i <= 2025; i++) {
+//			System.out.println(i);
+//		}
 	}
 }
